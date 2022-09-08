d64cb5679887ba2d49c4ef14590dfcf35045090f
Iago Goncalves <iago.g.goncalves@gmail.com>
2022-09-08 19:18:34 -0300

    aula 08/09 inicio exercicio pizza

---
+++ iago/pizza - exercicios/ex07.php
@@ -0,0 +1,11 @@
+<!-- 
+02 - Retirada de dinheiro caixa eletrônico.
+Tela principal terá que ter as opções consulta de saldo e retirada de valor.
+A tela de consulta, irá mostrar o saldo atual do cliente. (5k);
+A tela de retirada deve ter a opção do cliente colocar o valor que ele queira tirar.
+Na próxima tela, primeiramente precisa verificar se o cliente tem saldo suficiente
+para fazer a retirada do valor indicado. Caso tenha, deverá mostrar as notas que
+o caixa eletrônico tem disponível (um array, que pode variar dentre as notas
+disponíveis); Próximo passo, o sistema deve mostrar a quantidade de notas que o
+cliente irá receber e o valor que ele informou. NO final, o sistema deverá mostrar
+o saldo atual que existe na conta; -->
