64a2d078f4242d9744580db0a43e5226e13399a5
Iago Goncalves <iago.g.goncalves@gmail.com>
2022-09-29 00:09:58 -0300

    ex03-boletos / date.php

---
+++ iago/classe date/ex02.php
@@ -0,0 +1,19 @@
+<!-- /** 2 - Tela - fazer uma tela principal, onde deve ter entrada de dados do valor do boleto
+ * e data de vencimento.
+ * Backend - verificar se boleto está sendo pago no dia. Se estiver, mostrar na tela o valor 
+ * original.
+ * Se tiver em atraso, verificar a quantidade de dias em atraso, aplicar juros de 10% do valor
+ * total MAIS 1,50 por dia de atraso.
+ * Se tiver pagando pelo menos 7 dias adiantado, aplicar um desconto de 10% do valor, e a quantidade
+ * de dias que está pagando adiantado.
+ * 
+ */ -->
+
+
+
+<form action="./ex02-back.php
+" method="post">
+    <input type="number" name="valor">
+    <input type="date" name="data">
+    <button type="submit">Verificar</button>
+</form>
